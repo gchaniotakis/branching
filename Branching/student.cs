90ec9706581ad31619d638370fc71be4be5abc7c
George Chaniotakis <haniotinho@gmail.com>
2018-11-21 20:45:46 +0200

    added student branch/class/constructor

---
+++ Branching/student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching
+{
+    class student
+    {
+        public  string Name { get; set; }
+
+        public student (string name)
+        {
+            name = Name;
+        }
+    }
+}
